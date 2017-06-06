ef311a96708a9ba97b95b43be11913ab4a258b48
Yogev Sitton <yogev_sitton@intuit.com>
2017-06-06 14:28:42 +0300

    Initial commit (project file and Hebrew translation for String)

---
+++ HebSwift/HebSwift.h
@@ -0,0 +1,19 @@
+//
+//  HebSwift.h
+//  HebSwift
+//
+//  Created by Sitton, Yogev on 6/6/17.
+//  Copyright © 2017 YSitton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HebSwift.
+FOUNDATION_EXPORT double HebSwiftVersionNumber;
+
+//! Project version string for HebSwift.
+FOUNDATION_EXPORT const unsigned char HebSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HebSwift/PublicHeader.h>
+
+
---
+++ HebSwift/מחרוזת.swift
@@ -0,0 +1,18 @@
+//
+//  מחרוזת.swift
+//  HebSwift
+//
+//  Created by Sitton, Yogev on 6/6/17.
+//  Copyright © 2017 YSitton. All rights reserved.
+//
+
+import Foundation
+
+struct מחרוזת {
+    private var string : String
+    
+    init(_ string: String = "") {
+        self.string = string
+    }
+    
+}
